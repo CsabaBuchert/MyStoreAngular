06d401e79d3438465f1d389b487d6650fb43a396
csbuchert <csaba.buchert@nng.com>
2021-10-06 16:57:41 +0200

    Add submit form

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-order',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
+  cart: Map<number, number> = new Map();
+  full_name: string = '';
+  address: string = '';
+  credit_card: number = 0;
 
-  constructor() { }
+  @Output() submitOrder: EventEmitter<any> = new EventEmitter();
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cart = this.cartService.get();
   }
 
+  submitForm(): void {
+    this.submitOrder.emit();
+  }
 }
