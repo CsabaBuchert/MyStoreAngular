2deb886773392750ba63fc66a3e3e9a96136bf6d
csbuchert <csaba.buchert@nng.com>
2021-10-07 15:09:35 +0200

    fix credit card number checking

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartService } from '../cart.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-order',
@@ -10,18 +12,23 @@ export class OrderComponent implements OnInit {
   cart: Map<number, number> = new Map();
   full_name: string = '';
   address: string = '';
-  credit_card: number = 0;
+  credit_card: string = '';
 
   @Output() submitted = new EventEmitter();
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
     this.cart = this.cartService.getAll();
   }
 
   onSubmit() {
+    if (!this.credit_card.match(/^[0-9]+$/)) {
+      alert('Invalid credit card number!');
+      return;
+    }
     this.submitted.emit();
+    this.router.navigateByUrl('/order_completed');
   }
 
   isEmpty(): boolean {
